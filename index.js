decfc5420f8162d98fd6bc7e0e181bc8541fe6b0
Markus Fürer <m.fuerer@vtxmail.ch>
2022-09-14 16:19:09 +0200

    feat:

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 'use strict';
 
-const {logInfo, logError} = require('logger');
+const { logInfo, logError } = require('logger');
+const mariadb = require('mariadb');
+
 let pool = null;
 
-const getConnection = async () => {
-  if (!pool) {
-    const mariadb = require('mariadb');
+const config = {
+  host: process.env.MARIADB_SERVICE_HOST || 'localhost',
+  port: process.env.MARIADB_SERVICE_PORT || 3306,
+  user: process.env.MARIADB_USER || 'root',
+  password: process.env.MARIADB_PASSWORD || 'root',
+  database: process.env.MARIADB_NAME || 'omnimanager',
+};
 
-    pool = mariadb.createPool({
-      host: process.env.MARIADB_SERVICE_HOST || 'localhost',
-      port: process.env.MARIADB_SERVICE_PORT || 3306,
-      user: process.env.MARIADB_USER || 'root',
-      password: process.env.MARIADB_PASSWORD || 'root',
-      database: process.env.MARIADB_NAME || 'omnimanager',
-    });
+const getConnection = async (withPool = false) => {
+  let conn = null;
+
+  if (withPool) {
+    if (!pool) {
+      logInfo('create pool');
+      pool = mariadb.createPool(config);
+    }
+
+    logInfo('get connection from pool');
+    conn = await pool.getConnection();
+  } else {
+    logInfo('create connection');
+    conn = await mariadb.createConnection(config);
   }
-  logInfo('connect');
-  const conn = await pool.getConnection();
+
   return {
     query: (sql, bind) => {
       logInfo(sql, bind);
       return conn.query(sql, bind);
     },
     end: () => {
-      logInfo('end');
+      logInfo('connection end');
       return conn.end();
     },
   };
@@ -32,15 +44,15 @@ const getConnection = async () => {
 const handler = (getData) => async (req, res) => {
   let conn = null;
   try {
-    conn = await getConnection();
+    conn = await getConnection(true);
     const data = await getData(conn, req, res);
-    res.send(data || {state: 'ok'});
+    res.send(data || { state: 'ok' });
   } catch (err) {
     await logError(err.message);
     res.status(500).send(err);
   } finally {
     if (conn) {
-      conn.end();
+      await conn.end();
     }
   }
 };
